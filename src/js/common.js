8eeb8bebdfb2599894e8e87a98f508dfebe20820
Aleksey Karasev <karasev.dev@gmail.com>
2018-08-22 22:54:34 +0300

    add mousewheel js

--- src/js/common.js
+++ src/js/common.js
@@ -399,4 +399,37 @@ $(function () {
         clusterer.add(geoObjects);
 
     }
+
+    function mouseWheel() {
+        var divs = $('.section');
+        var dir = 'up';
+        var div = 0;
+        $(document.body).on('DOMMouseScroll mousewheel', function (e) {
+            if (e.originalEvent.detail > 0 || e.originalEvent.wheelDelta < 0) {
+                dir = 'down';
+            } else {
+                dir = 'up';
+            }
+            div = -1;
+            divs.each(function (i) {
+                if (div < 0 && ($(this).offset().top >= $(window).scrollTop())) {
+                    div = i;
+                }
+            });
+            if (dir == 'up' && div > 0) {
+                div--;
+            }
+            if (dir == 'down' && div < divs.length) {
+                div++;
+            }
+            $('html,body').stop().animate({
+                scrollTop: divs.eq(div).offset().top
+            }, 600);
+            return false;
+        });
+        $(window).resize(function () {
+            $('html,body').scrollTop(divs.eq(div).offset().top);
+        });
+    }
+    mouseWheel();
 });
