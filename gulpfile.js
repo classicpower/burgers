9ba5b97f31145771dc1f253f3cbebfe8a44e0034
Aleksey Karasev <karasev.dev@gmail.com>
2018-08-22 16:10:07 +0300

    add gulp plugin, optimize img

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,10 @@ const
     autoprefixer = require('gulp-autoprefixer'),
     browserSync = require('browser-sync').create(),
     sass = require('gulp-sass'),
-    gcmq = require('gulp-group-css-media-queries');
-// cleanCSS = require('gulp-clean-css'),
-// sourcemaps = require('gulp-sourcemaps'),
+    imagemin = require('gulp-imagemin'),
+    gcmq = require('gulp-group-css-media-queries'),
+    cleanCSS = require('gulp-clean-css'),
+    sourcemaps = require('gulp-sourcemaps');
 // rename = require('gulp-rename');
 
 const config = {
@@ -20,33 +21,54 @@ const config = {
     },
     js: {
         src: '/js/*.js'
+    },
+    img: {
+        src: '/img/**/*',
+        dest: '/img/'
     }
 };
 
 gulp.task('build', function () {
     gulp.src(config.src + config.css.src)
-        // .pipe(sourcemaps.init())
+        .pipe(sourcemaps.init())
         .pipe(sass({
             includePaths: require('node-normalize-scss').includePaths
         }).on('error', sass.logError))
-        // .pipe(gcmq())
+        .pipe(gcmq())
         .pipe(autoprefixer({
             browsers: ['last 15 version', '> 0.1%'],
             cascade: false
         }))
-        // .pipe(cleanCSS({
-        //     level: 2
-        // }))
-        // .pipe(sourcemaps.write('.'))
-        .pipe(gcmq())
+        .pipe(cleanCSS({
+            level: 2
+        }))
+        .pipe(sourcemaps.write('.'))
+        // .pipe(gcmq())
         .pipe(gulp.dest(config.src + config.css.dest))
         .pipe(browserSync.reload({
             stream: true
         }));
 });
+gulp.task('image', function () {
+    gulp.src(config.src + config.img.src)
+        .pipe(imagemin([
+            imagemin.gifsicle({ interlaced: true }),
+            imagemin.jpegtran({ progressive: true }),
+            imagemin.optipng({ optimizationLevel: 5 }),
+            imagemin.svgo({
+                plugins: [
+                    { removeViewBox: true },
+                    { cleanupIDs: false }
+                ]
+            })
+        ]))
+        .pipe(gulp.dest(config.src + config.img.dest))
+}
+);
 
 gulp.task('watch', ['browserSync'], function () {
     gulp.watch(config.src + config.css.watch, ['build']);
+    gulp.watch(config.src + config.img.src, ['image']);
     gulp.watch(config.src + config.html.src, browserSync.reload);
     gulp.watch(config.src + config.js.src, browserSync.reload);
 });
